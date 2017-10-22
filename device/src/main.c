350489830ba8d50e87aea5692f43be2b751483b5
Geektree0101 <h2s1880@gmail.com>
2017-10-22 16:11:51 +0900

    start coding test

--- device/src/main.c
+++ device/src/main.c
@@ -18,14 +18,22 @@ void setup() {
 void loop() {
     // TODO: Device work
 
-    int a = 0;
-    for(int index = 0; index < 10; index++) {
-        a += index;
-    }
-    printf(a);
-
 
     hello_miro(); // call test
 }
+
+// TODO: 동영이는 target에서 source까지의 숫자를 전부 더하시오
+// ex: 2, 4 -> 2 + 3 + 4 -> 9
+int algorythm_test_dongyoung(int target, int source) {
+
+    return 0;
+}
+
+// TODO: 동호는  target에서 source까지의 숫자를 전부 곱하시오.
+int algorythm_test_ho(int target, int source) {
+    
+        return 0;
+}
+
 
 #endif
--- device/test/native_test/native_test.c
+++ device/test/native_test/native_test.c
@@ -13,13 +13,33 @@
 #include <unity.h>
 
 #ifdef UNIT_TEST 
+int algorythm_test_dongyoung(int target, int source);
+int algorythm_test_ho(int target, int source);
 
 int main(int argc, char **argv) {
     TEST_ASSERT_EQUAL(200, unit_test_adder(100, 100)); // unit test test
     TEST_ASSERT_EQUAL(UV_LED_OFF, ultra_violet_led_control(UV_LED_OFF));
     TEST_ASSERT_EQUAL(UV_LED_ON, ultra_violet_led_control(UV_LED_ON));
+
+    // TODO 테스트코드 작성하시오 2개 정도
+ 
     return 0;
 }
+
+
+// TODO: 동영이는 target에서 source까지의 숫자를 전부 더하시오
+// ex: 2, 4 -> 2 + 3 + 4 -> 9
+int algorythm_test_dongyoung(int target, int source) {
+    
+    return 0;
+}
+    
+    // TODO: 동호는  target에서 source까지의 숫자를 전부 곱하시오.
+int algorythm_test_ho(int target, int source) {
+        
+    return 0;
+}
+    
 
 #endif
 
