f7d493f3abbb029e650202ca1886e151153bc070
ehddud6652@likelion.org <ehddud6642@naver.com>
2017-10-22 16:25:03 +0900

    MIRO-test-yeong

--- device/test/native_test/native_test.c
+++ device/test/native_test/native_test.c
@@ -1,7 +1,7 @@
 //
 //  native_test.c
 //  Habiteeth
-//  
+//
 //  Created by Hyeon soo Ha on 2017. 10. 15..
 //  Copyright © 2017년 MIRO. All rights reserved.
 //
@@ -12,7 +12,7 @@
 
 #include <unity.h>
 
-#ifdef UNIT_TEST 
+#ifdef UNIT_TEST
 int algorythm_test_dongyoung(int target, int source);
 int algorythm_test_ho(int target, int source);
 
@@ -20,9 +20,9 @@ int main(int argc, char **argv) {
     TEST_ASSERT_EQUAL(200, unit_test_adder(100, 100)); // unit test test
     TEST_ASSERT_EQUAL(UV_LED_OFF, ultra_violet_led_control(UV_LED_OFF));
     TEST_ASSERT_EQUAL(UV_LED_ON, ultra_violet_led_control(UV_LED_ON));
-
+    TEST_ASSERT_EQUAL(10,algorythm_test_dongyoung(1,4));
     // TODO 테스트코드 작성하시오 2개 정도
- 
+
     return 0;
 }
 
@@ -30,16 +30,19 @@ int main(int argc, char **argv) {
 // TODO: 동영이는 target에서 source까지의 숫자를 전부 더하시오
 // ex: 2, 4 -> 2 + 3 + 4 -> 9
 int algorythm_test_dongyoung(int target, int source) {
-    
+    for(int i; i>source; i++){
+      target += 1;
+      return target;
+    }
+
     return 0;
 }
-    
+
     // TODO: 동호는  target에서 source까지의 숫자를 전부 곱하시오.
 int algorythm_test_ho(int target, int source) {
-        
+
     return 0;
 }
-    
+
 
 #endif
-
